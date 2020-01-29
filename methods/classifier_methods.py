9f92ba969392bfc3166caec01a24428411e51d43
sarah cameron <sarahcameron@sarahs-MBP.fios-router.home>
2020-01-28 21:49:29 -0500

    updated base class. added an executables folder to .gitignore

--- methods/classifier_methods.py
+++ methods/classifier_methods.py
@@ -7,45 +7,18 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 import logging
 
+from methods.classification_base import BaseClassifier
+
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-class Classification(object):
+class SVM(BaseClassifier):
 
     def __init__(
-            self,
-            df,
-            class_column,
-            split_level=0.2,
-            labels=None
+          self,
+          df, class_column, split_level=0.2, labels=None, kernel='rbf', class_weight='balanced'
     ):
-        """
-        Args:
-            df(pandas.core.DataFrame): queried DataFrame
-            class_column (str): the name of the column denoting the class
-            split_level (int): the percent to split the data into train/test.
-                               default is 80% train 20% test
-            labels(numpy.ndarray): class labels
-        """
-        self.df = df
-        self.class_column = class_column
-        self.split_level = split_level
-
-        try:
-            self.x = self.df.drop(self.class_column, axis=1)
-            self.y = self.df[self.class_column]
-        except (KeyError, Exception) as e:
-            logger.error("{}:".format(type(e)), exc_info=True)
-            logger.error("Column Name not in DataFrame")
-            raise
-
-        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
-            self.x, self.y, test_size=self.split_level)
-        self.labels = labels
-
-    @property
-    def svm(self, kernel='rbf', class_weight='balanced'):
         """
         A Support Vector Machine Classifier
         Args:
@@ -57,43 +30,46 @@ class Classification(object):
             cm(numpy.ndarray): the confusion matrix values
             roc_auc(in): the ROC value indicating classifier performance
         """
-        svclassifier = SVC(kernel=kernal, class_weight=class_weight)
-        svclassifier.fit(self.x_train, self.y_train)
-        y_pred_svc = svclassifier.predict(self.x_test)
-        cm, roc_auc = evaluate_classification(
-            y_pred_svc, self.y_test, self.labels, method='SVC')
-        return y_pred_svc, cm, roc_auc
+        self.kernel = kernel
+        self.class_weight=class_weight
 
-    def KNN(self, neighbors=5):
-        """
-        A K Nearest Neighbors Classifier
-        Args:
-            neighbors (int): The number of neighbors to use
-        Returns:
-            y_pred (numpy.ndarray): the classification predictions
-            cm(numpy.ndarray): the confusion matrix values
-            roc_auc(in): the ROC value indicating classifier performance
-        """
-        model = KNeighborsClassifier(n_neighbors=neighbors)
-        model.fit(self.x_train, self.y_train)
-        y_pred_knn = model.predict(self.x_test)
-        cm, roc_auc = evaluate_classification(
-            y_pred_knn, self.y_test, self.labels, method='KNN')
-        return y_pred_knn, cm, roc_auc
+        super(SVM, self).__init__(
+            df = df,
+	        class_column = class_column,
+	        split_level = split_level,
 
-    def GaussNB(self):
-        """
-        A NaiveBayes Classifier
-        Args:
-             None
-        Returns:
-            y_pred (numpy.ndarray): the classification predictions
-            cm(numpy.ndarray): the confusion matrix values
-            roc_auc(in): the ROC value indicating classifier performance
-        """
-        clf = GaussianNB()
-        clf.fit(self.x_train, self.y_train)
-        y_pred = clf.predict(self.x_test)
-        cm, roc_auc = evaluate_classification(
-            y_pred, self.y_test, self.labels, method='Naive Bayes')
-        return y_pred, cm, roc_auc
+        )
+
+    # def KNN(self, neighbors=5):
+    #     """
+    #     A K Nearest Neighbors Classifier
+    #     Args:
+    #         neighbors (int): The number of neighbors to use
+    #     Returns:
+    #         y_pred (numpy.ndarray): the classification predictions
+    #         cm(numpy.ndarray): the confusion matrix values
+    #         roc_auc(in): the ROC value indicating classifier performance
+    #     """
+    #     model = KNeighborsClassifier(n_neighbors=neighbors)
+    #     model.fit(self.x_train, self.y_train)
+    #     y_pred_knn = model.predict(self.x_test)
+    #     cm, roc_auc = evaluate_classification(
+    #         y_pred_knn, self.y_test, self.labels, method='KNN')
+    #     return y_pred_knn, cm, roc_auc
+
+    # def GaussNB(self):
+    #     """
+    #     A NaiveBayes Classifier
+    #     Args:
+    #          None
+    #     Returns:
+    #         y_pred (numpy.ndarray): the classification predictions
+    #         cm(numpy.ndarray): the confusion matrix values
+    #         roc_auc(in): the ROC value indicating classifier performance
+    #     """
+    #     clf = GaussianNB()
+    #     clf.fit(self.x_train, self.y_train)
+    #     y_pred = clf.predict(self.x_test)
+    #     cm, roc_auc = evaluate_classification(
+    #         y_pred, self.y_test, self.labels, method='Naive Bayes')
+    #     return y_pred, cm, roc_auc
